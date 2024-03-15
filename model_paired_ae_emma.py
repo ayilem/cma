17338f65a5e9d32ec872bebe003b8de2c72e831c
Emma Mendizabal <e.mendizabal@orange.fr>
2024-03-15 14:06:51 +0100

    model

--- model_paired_ae_emma.py
+++ model_paired_ae_emma.py
@@ -18,18 +18,15 @@ class Encoder(nn.Module):
     
         return out
     
-
 class Decoder(nn.Module):
     
     def __init__(self, n_input, latent_dims, n_hidden):
         super().__init__()
         self.decoder = nn.Sequential(
             nn.Linear(latent_dims, n_hidden),
-            #nn.BatchNorm1d(n_hidden),
             nn.ReLU(inplace=True),
             
             nn.Linear(n_hidden, n_hidden),
-            #nn.BatchNorm1d(n_hidden),
             nn.ReLU(inplace=True),
             
             nn.Linear(n_hidden, n_input),
@@ -37,25 +34,18 @@ class Decoder(nn.Module):
 
     def forward(self, z):
         return self.decoder(z)
-  
-
 
 
 class MultimodalAE(nn.Module):
     def __init__(self, encoder, decoder):
         super(MultimodalAE, self).__init__()
 
-        # Encodeurs pour les modalités Expression et Methylation
         self.encoder = encoder
-
-        # Décodeur pour les modalités Expression et Methylation
         self.decoder = decoder
 
 
     def forward(self, x):
-        # Encode les modalités Expression et Methylation
         z = self.encoder(x)
-
-        # Décode les modalités Expression et Methylation + les échanges de modalités
         o = self.decoder(z)
+        
         return o
