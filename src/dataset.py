290cc0af4a46569d5b763d6128099e5cb5b290fd
Emma Mendizabal <e.mendizabal@orange.fr>
2024-05-04 19:54:54 +0200

    add all files updated

--- src/dataset.py
+++ src/dataset.py
@@ -3,9 +3,10 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
-root_dir = os.path.join(os.path.dirname(__file__), '../data')
 
+root_dir = os.path.join(os.path.dirname(__file__), '../raw') 
 
 class IntersimDataset(Dataset):
     def __init__(self, data, labels):
@@ -18,15 +19,14 @@ class IntersimDataset(Dataset):
     def __getitem__(self, idx):
         return self.df[idx], self.labels[idx]
 
-def get_paired_data(suffix):
-    expr_X = pd.read_csv(root_dir + '/paired/expression_' + suffix + '.txt', sep='\t')
-    methyl_X = pd.read_csv(root_dir + '/paired/methylation_' + suffix + '.txt', sep='\t')
-    protein_X = pd.read_csv(root_dir + '/paired/protein_' + suffix + '.txt', sep='\t')
 
-    y_common = pd.read_csv(root_dir + '/paired/clusters_' + suffix + '.txt', sep='\t', index_col=0)
+def get_paired_data(suffix, train_or_test):
+        expr_X = pd.read_csv(root_dir + '/paired/' + suffix + '/' + train_or_test + '_gene_expression_' + suffix + '_paired.txt', sep='\t')
+        methyl_X = pd.read_csv(root_dir + '/paired/' + suffix + '/' + train_or_test + '_dna_methylation_' + suffix + '_paired.txt', sep='\t')
+        protein_X = pd.read_csv(root_dir + '/paired/' + suffix + '/' + train_or_test + '_protein_expression_' + suffix + '_paired.txt', sep='\t')
+        y_common = pd.read_csv(root_dir + '/paired/' + suffix + '/' + train_or_test + '_labels_' + suffix + '_paired.txt', sep='\t')
 
-    return (expr_X, y_common), (methyl_X, y_common), (protein_X, y_common)
-
+        return (expr_X, y_common), (methyl_X, y_common), (protein_X, y_common)
 
 def get_unpaired_data(suffix):
     expr_X = pd.read_csv(root_dir + '/unpaired/expression_unpaired_' + suffix + '.txt', sep='\t')
@@ -41,7 +41,7 @@ def get_unpaired_data(suffix):
     return (expr_X, expr_y), (methyl_X, methyl_y), (protein_X, protein_y)
 
 
-def concat_datasets(root_dir = root_dir, suffix = 'diff', type='paired'):
+def concat_datasets(root_dir = root_dir, suffix = '3_clusters', type='paired'):
     # Noms des colonnes
 
     data_types = ['clusters', 'expression', 'methylation', 'protein']
@@ -61,28 +61,33 @@ def concat_datasets(root_dir = root_dir, suffix = 'diff', type='paired'):
     return "Concatenated paired datasets : Done"
 
 
-from sklearn.preprocessing import StandardScaler
-
-def generate_datasets(suffix='5_diff', type='unpaired', train=True, test=False):
+def generate_datasets(suffix='3_clusters', type='paired', train=True, test=False):
     if type == 'paired':
-        expr, methyl, protein = get_paired_data(suffix)
-        print('Loading paired dataset')
-    elif type == 'unpaired':
-        expr, methyl, protein = get_unpaired_data(suffix)
-        print('Loading unpaired dataset')
+        expr_train, methyl_train, protein_train = get_paired_data(suffix, train_or_test = 'train')
+        print('Loading train paired dataset')
+        expr_test, methyl_test, protein_test = get_paired_data(suffix, train_or_test = 'test')
+        print('Loading test paired dataset')
     else:
         raise Exception('Invalid dataset type')
 
-    expr_X, expr_y = expr
-    methyl_X, methyl_y = methyl
-    protein_X, protein_y = protein
+    expr_train_X, expr_train_y = expr_train
+    expr_train_y = expr_train_y.iloc[:, 1].astype(int)
+    methyl_train_X, methyl_train_y = methyl_train
+    methyl_train_y = methyl_train_y.iloc[:, 1].astype(int)
+    protein_train_X, protein_train_y = protein_train
+    protein_train_y = protein_train_y.iloc[:, 1].astype(int)
 
-    expr_train_X, expr_test_X, expr_train_y, expr_test_y = train_test_split(expr_X, expr_y, test_size=0.2, random_state=53)
-    methyl_train_X, methyl_test_X, methyl_train_y, methyl_test_y = train_test_split(methyl_X, methyl_y, test_size=0.2, random_state=53)
-    protein_train_X, protein_test_X, protein_train_y, protein_test_y = train_test_split(protein_X, protein_y, test_size=0.2, random_state=53)
+    expr_test_X, expr_test_y = expr_test
+    expr_test_y = expr_test_y.iloc[:, 1].astype(int)
+    methyl_test_X, methyl_test_y = methyl_test
+    methyl_test_y = methyl_test_y.iloc[:, 1].astype(int)
+    protein_test_X, protein_test_y = protein_test
+    protein_test_y = protein_test_y.iloc[:, 1].astype(int)
 
     # Normaliser les données d'entraînement et de test
-    scaler = StandardScaler()
+    import pickle
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    
     expr_train_X = scaler.fit_transform(expr_train_X)
     expr_test_X = scaler.transform(expr_test_X)
 
@@ -91,6 +96,10 @@ def generate_datasets(suffix='5_diff', type='unpaired', train=True, test=False):
 
     protein_train_X = scaler.fit_transform(protein_train_X)
     protein_test_X = scaler.transform(protein_test_X)
+
+    with open('scaler.pkl', 'wb') as f:
+        pickle.dump(scaler, f)
+        print('Scaler saved')
 
     exp_train_dataset = IntersimDataset(expr_train_X, expr_train_y)
     exp_test_dataset = IntersimDataset(expr_test_X, expr_test_y)
@@ -108,5 +117,4 @@ def generate_datasets(suffix='5_diff', type='unpaired', train=True, test=False):
         datasets.extend([exp_test_dataset, methyl_test_dataset, protein_test_dataset])
     
     return datasets
-
 
